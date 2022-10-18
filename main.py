b2530d8a38705a21fef5abf1f7252094bdb25cd8
AbhisekSwain53 <115418054+AbhisekSwain53@users.noreply.github.com>
2022-10-18 15:30:56 +0530

    Basics of python

---
+++ main.py
@@ -0,0 +1,53 @@
+# # # # # number = int(input("Enter a number of your own choice!: "))
+# # # # # age = int(input("What is your age?"))
+# # # # # if number % 2 == 0:
+# # # # #   print("This is an Even Number !")
+# # # # #   if age < 12:
+# # # # #     print("Pay $20 only")
+# # # # #   elif age >= 12:
+# # # # #     print("Pay $40 only")
+# # # # #   elif age < 50:
+# # # # #     print("pay $60 only")
+# # # # # else:
+# # # # #   print("This is an Odd Number !")
+# # # # height = int(input("Please enter your height in cm :-"))
+
+# # # # if height >= 120:
+# # # #   print("You can enjoy our rollercoaster ride!")
+# # # #   age = int(input("PLease enter your age:-"))
+# # # #   if age < 12:
+# # # #     pr()
+# # # #   elif age <=18:
+# # # #     print("Please pay $7.")
+# # # #   else:
+# # # #     print("Please pay $12.")
+# # # # else:
+# # # #   print("You have to grow taller to ride our rollercoaster.")
+# # # height = float(input("Enter your height in m:- "))
+# # # weight = int(input("Enter your weight in kg:-"))
+# # # bmi = round(weight / height**2 ,2)
+# # # if bmi <= 18.5:
+# # #   print(f"Your bmi is {bmi} so you are underweight.")
+# # # elif bmi <= 25:
+# # #   print(f"Your bmi is {bmi} so you are normal.")
+# # # elif bmi <= 30:
+# # #   print(f"Your bmi is {bmi} so you are overweight.")
+# # # elif bmi <= 35:
+# # #   print(f"Your bmi is {bmi} so you are obese.")
+# # # else:
+# # #   print(f"Your bmi is {bmi} so you are clinically obese.")
+# # year = int(input("Which year do you want to check?"))
+
+# # if year % 4 == 0:
+# #   if year % 100 != 0:
+# #     print("It is a leap year.")
+# #   elif year % 400:
+# #     print("It is a leap year.")
+# # else:
+# #   print("It is a non leap year
+
+# # print(type(number))
+# number = input("Please Enter a Two Digit Number:")
+# Addition = int(number[0]) + int(number[1])
+# print(Addition)
+print(round(8 / 3, 2))
